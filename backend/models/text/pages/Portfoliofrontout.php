45671d10ec4129889e3f3ab4196f8320829bc285
Anatoliy Krekotenko <krekotenko@gmail.com>
2016-05-13 15:11:12 +0300

    add_portfoliofronoutpage

---
+++ backend/models/text/pages/Portfoliofrontout.php
@@ -0,0 +1,120 @@
+<?php
+namespace backend\models\text\pages;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * Portfoliofrontout
+ */
+
+Class Portfoliofrontout extends Model
+{
+    
+    public function getPageByIdAndLang($id,$lang) {
+        $query = Yii::$app->db->createCommand('SELECT
+            `a`.`id`, `a`.`pShow`, `a`.`pUrl`,
+            `b`.`pTitle`, `b`.`pDescription`, `b`.`pKeyWords`, `b`.`pH1`,
+            `b`.`pMenuName`, `b`.`pBreadCrumbs`, `b`.`pContent`
+			/*get*/
+        FROM
+            `pages` `a`, `content` `b`
+        WHERE
+            `a`.`id` = :id AND `a`.`id` = `b`.`pageId` AND `b`.`lang` = :lang')
+        ->bindValue(':id', $id)
+	->bindValue(':lang', $lang);
+        
+        return $query->queryAll();
+    }
+    
+    public function getEmptyLangPageById($id)
+    {
+        $query = Yii::$app->db->createCommand('SELECT
+        `a`.`id`, `a`.`pShow`, `a`.`pUrl`,
+         "" AS `pTitle`, "" AS `pDescription`, "" AS `pKeyWords`, "" AS `pH1`, "" AS `pMenuName`, "" AS `pBreadCrumbs`, "" AS `pContent`
+		FROM
+            `pages` `a`
+        WHERE
+            `a`.`id` = :id')
+        ->bindValue(':id', $id);
+        
+        return $query->queryAll();
+    }
+
+	public function editUpDatePage($pageId, $lang, $pTitle, $pDescription, $pKeyWords, $pH1, $pMenuName, $pBreadCrumbs, $pContent) {
+        $query = Yii::$app->db->createCommand('UPDATE `content`
+        SET `pTitle` = :pTitle, `pDescription` = :pDescription, `pKeyWords` = :pKeyWords, `pH1` = :pH1,
+            `pMenuName` = :pMenuName, `pBreadCrumbs` = :pBreadCrumbs, `pContent` = :pContent
+        WHERE `pageId` = :pageId AND `lang` = :lang')
+        ->bindValue(':pageId', $pageId)
+        ->bindValue(':lang', $lang)
+        ->bindValue(':pTitle', $pTitle)
+        ->bindValue(':pDescription', $pDescription)
+        ->bindValue(':pKeyWords', $pKeyWords)
+        ->bindValue(':pH1', $pH1)
+        ->bindValue(':pMenuName', $pMenuName)
+        ->bindValue(':pBreadCrumbs', $pBreadCrumbs)        
+        ->bindValue(':pContent', $pContent);
+        
+        return $query->execute();
+    }
+	public function update($id, $postBase, $postContent, $lang) {
+		$data = $postBase;
+		
+		if (isset($postContent)) {
+			$data = array_merge($data, $postContent);
+		}
+		
+		if (empty($data)) return false;
+		
+		$set = array();
+		foreach ($data as $key => $item) {
+			$set[] = '`'.$key.'`="'.addslashes($item).'"';
+		}
+		$set = implode(',', $set);
+		
+		$query = Yii::$app->db->createCommand('UPDATE
+			`pages`, `content`
+		SET
+			'.$set.'
+		WHERE
+			`id` = :id AND
+			`pageId` = `id` AND
+			`lang` = :lang')
+		->bindValue(':id', $id)
+        ->bindValue(':lang', $lang);
+		
+		return $query->execute();
+	}
+    
+    public function getAllLangs()
+    {
+        $query = Yii::$app->db->createCommand('SELECT l.sName, l.bName, l.fullName FROM langs l ORDER BY l.id ASC');
+        return $query->queryAll();
+    }
+    
+    public function getLangPageIs($id, $lang)
+    {
+        $query = Yii::$app->db->createCommand('SELECT count(*) AS `count`
+           FROM
+                content c
+           WHERE
+             c.pageId = :id
+             AND c.lang = :lang')
+        ->bindValue(':id', $id)
+	->bindValue(':lang', $lang);
+        
+        $count = $query->queryOne();
+        return $count['count'];
+    }
+    
+    public function addLangPage($id, $lang)
+    {
+        $query = Yii::$app->db->createCommand('INSERT INTO `content`
+            (`pageId`, `lang`, `pH1`, `pContent`) VALUES (:pageId, :lang, "", "")')
+            ->bindValue(':pageId', $id)
+            ->bindValue(':lang', $lang);
+        
+        return $query->execute();
+    }
+}// End     
---
+++ backend/views/pages/PortfoliofrontoutPageEditView.php
@@ -0,0 +1,39 @@
+<?php
+use yii\helpers\Html;
+
+//Навигационное меню НАЧАЛО
+$navMenu = '<nav class="sidebar__menu">';
+if (is_file(Yii::$app->basePath.'/views/parts/LangsView.php')) {
+    require Yii::$app->basePath.'/views/parts/LangsView.php';
+}
+if (is_file(Yii::$app->basePath.'/views/parts/TwoButtonsSPView.php')) {
+    require Yii::$app->basePath.'/views/parts/TwoButtonsSPView.php';
+}
+$navMenu .= '</nav>';
+//Навигационное меню КОНЕЦ
+
+//Хлебные крошки НАЧАЛО
+$content = '<ul class="crumbs">
+                <li class="crumbs__item"><a href="/'.$id1Uri.'/'.$defLang.'" data-href="formlist">'.$pageGroupData[0]['groupName'].'</a></li>
+                <li class="crumbs__item crumbs__item-active">'.$textPageHeader.'</li>
+            </ul>';
+//Хлебные крошки КОНЕЦ/* UpdateCode */
+
+$content .= Html::beginForm($admPanelUri.'formupdate/'.$id1Uri.'/'.$page[0]['id'].'/'.$pageLang, 'post', ['id'=>"form-edit-content"]).
+                '<!-- sectionPageData --><fieldset class="catalog__section">
+                '.$this->createHeader('Основные данные страницы').'
+                    <div class="catalog__section-data">
+                    <!-- pH1 -->'.$this->createInput(['id'=> 'pH1', 'text' => 'Заголовок новости H1', 'width' => 400, 'name' => 'pH1', 'value' => $page[0]['pH1'], 'attr' => 'required autofocus autocomplete="off"']).'<!-- /pH1 -->
+					<!-- pTitle -->'.$this->createInput(['id'=> 'pTitle', 'text' => 'Заголовок страницы', 'placeholder' => 'В поисковой выдаче видно 60 символов', 'width' => 400, 'name' => 'pTitle', 'value' => $page[0]['pTitle'], 'attr' => 'required data-count="60"', 'dataCopy' => 'pH1', 'titleCopy' => 'Копия заголовка H1']).'<!-- /pTitle -->
+                    <!-- pDescription -->'.$this->createTextArea(['id'=> 'pDescription', 'text' => 'Meta description', 'placeholder' => 'В поисковой выдаче видно 140 символов', 'width' => '400x100', 'name' => 'pDescription', 'value' => $page[0]['pDescription'], 'attr' => 'data-count="140"']).'<!-- /pDescription -->
+                    <!-- pKeyWords -->'.$this->createInput(['id'=> 'pKeyWords', 'text' => 'Meta keywords', 'width' => 400, 'name' => 'pKeyWords', 'value' => $page[0]['pKeyWords'], 'attr' => '']).'<!-- /pKeyWords -->
+                    <!-- pMenuName -->'.$this->createInput(['id'=> 'pMenuName', 'text' => 'Заголовок для главного меню', 'width' => 400, 'name' => 'pMenuName', 'value' => $page[0]['pMenuName'], 'attr' => 'required']).'<!-- /pMenuName -->
+                    <!-- pBreadCrumbs -->'.$this->createInput(['id'=> 'pBreadCrumbs', 'text' => 'Хлебные крошки', 'width' => 400, 'name' => 'pBreadCrumbs', 'value' => $page[0]['pBreadCrumbs'], 'attr' => 'required']).'<!-- /pBreadCrumbs -->
+                    </div>
+                </fieldset>
+
+                <fieldset class="catalog__section">
+                '.$this->createHeader('Редактирование контента страницы').'
+                <!-- pContent -->'.$this->createTextArea(['id'=> 'pContent', 'text' => '', 'width' => '400x100', 'name' => 'pContent', 'value' => $page[0]['pContent'], 'attr' => 'data-module="tinymce"']).'<!-- /pContent -->
+                </fieldset><!-- /sectionPageData --><!-- /createFinish -->'.
+            Html::endForm();
